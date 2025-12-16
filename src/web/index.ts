2d6d99c4622c195682444f819990f69c8be812ed
Christoph Braun <christoph.braun@protonmail.com>
2025-12-16 16:55:34 +0100

    expose IndexedDB-based SessionDatabase

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,5 +1,6 @@
 export { WebWorkerSession as Session } from './Session';
 export { SessionEvents } from '../core/Session';
+export { SessionIDB } from './SessionDatabase';
 export type { WebWorkerSessionOptions as SessionOptions } from './Session';
 export type { SessionStateChangeDetail, SessionExpirationWarningDetail } from '../core/Session';
 export type { DereferencableIdClientDetails, DynamicRegistrationClientDetails } from '../core/SessionInformation';
